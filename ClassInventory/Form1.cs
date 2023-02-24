945098e18b43ac367d6686343f92201ea760131a
abigcatc683 <abigcatc683@SDS-D-114WHS>
2023-02-24 10:02:33 -0500

    Started

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -13,7 +13,8 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // Global Variabales go here
-
+        List<Player> players = new List<Player>();
+        //List<age> Age = new List<age>(new int[]);
 
         public Form1()
         {
@@ -22,12 +23,30 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            string name, team, position;
+            int age = Convert.ToInt32(ageInput.Text);
 
+            name = nameInput.Text;
+            team = teamInput.Text;
+            position = positionInput.Text;
+
+            Player newPlayer = new Player(name, age, team, position);
+            newPlayer.name = name;
+
+            displayPlayers();
         }
 
         private void removeButton_Click(object sender, EventArgs e)
         {
+            foreach (Player p in players)
+            {
+                if (p.name == nameInput.Text)
+                {
+                    players.Remove(p);
+                }
+            }
 
+            displayPlayers();
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -41,6 +60,16 @@ namespace ClassInventory
         private void showButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void displayPlayers()
+        {
+            outputLabel.Text = "";
+
+            foreach (Player p in players)
+            {
+                outputLabel.Text += $"{p.name} {p.age} {p.team} {p.position}\n";
+            }
         }
     }
 }
